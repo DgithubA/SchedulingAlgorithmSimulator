5bb6968fc0f576ba401b340ff31d065dd600b0b6
DANA <bibaks34@gmail.com>
2025-01-29 00:41:04 +0330

    chore(config): update Next.js configuration for production environment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? '/SchedulingAlgorithmSimulator/' : '',
+  basePath: isProd ? '/SchedulingAlgorithmSimulator' : '',
+  output: 'export',
+  eslint: {ignoreDuringBuilds: true},
+};
 
 export default nextConfig;
