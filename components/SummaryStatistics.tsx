ca80878d91ccab023b14c28c2c3f6b3cb6cddbbe
DANA <bibaks34@gmail.com>
2025-01-31 19:23:59 +0330

    feat(SummaryStatistics): integrate Tooltip component for enhanced statistics display

--- components/SummaryStatistics.tsx
+++ components/SummaryStatistics.tsx
@@ -1,8 +1,7 @@
 import { Process } from "@/lib/Process";
 import { turnaroundTime, waitingTime, cpuUtilization, totalExecutionTime } from "./SummaryTable"
 import AnimatedShinyText from "./ui/animated-shiny-text";
-
-
+import { Tooltip } from "@heroui/tooltip";
 
 type totalProcessesType = {
   totalProcesses: number;
@@ -11,29 +10,36 @@ type totalProcessesType = {
 
 export default function SummaryStatistics({ totalProcesses, scheduledProcesses }: totalProcessesType) {
 
-    return (
-      <div className="flex flex-col justify-evenly items-center md:w-1/2 md:pl-10 pt-4">
-        <div className="flex justify-evenly w-full text-center">
+  return (
+    <div className="flex flex-col justify-evenly items-center md:w-1/2 md:pl-10 pt-4">
+      <div className="flex justify-evenly w-full text-center">
+        <Tooltip content="waitingTime / totalProcesses" delay={300}>
           <div className="text-sm md:text-lg">
             <AnimatedShinyText>Avg Waiting Time</AnimatedShinyText>
-
-            {Math.round((waitingTime / totalProcesses) * 100) / 100}
+            {waitingTime + '/' + totalProcesses + ' ≈ ' + Math.round((waitingTime / totalProcesses) * 100) / 100}
           </div>
+        </Tooltip>
+        <Tooltip content="turnaroundTime / totalProcesses" delay={300}>
           <div className="text-sm md:text-lg">
             <AnimatedShinyText>Avg Turnaround Time</AnimatedShinyText>
-            {Math.round((turnaroundTime / totalProcesses) * 100) / 100}
+            {turnaroundTime + '/' + totalProcesses + ' ≈ ' + Math.round((turnaroundTime / totalProcesses) * 100) / 100}
           </div>
-        </div>
-        <div className="flex justify-evenly w-full text-center">
+        </Tooltip>
+      </div>
+      <div className="flex justify-evenly w-full text-center">
+        <Tooltip content="totalProcesses / totalExecutionTime" delay={300}>
           <div className="text-sm md:text-lg">
             <AnimatedShinyText>Throughput</AnimatedShinyText>
-            {Math.round((totalProcesses / totalExecutionTime) * 100) / 100}
+            {totalProcesses + '/' + totalExecutionTime + ' ≈ ' + Math.round((totalProcesses / totalExecutionTime) * 100) / 100}
           </div>
+        </Tooltip>
+        <Tooltip content="totalBurstTime / totalExecutionTime" delay={300}>
           <div className="text-sm md:text-lg">
             <AnimatedShinyText>CPU Utilization</AnimatedShinyText>
             {Math.round(cpuUtilization * 100) / 100}%
           </div>
-        </div>
+        </Tooltip>
       </div>
-    );
+    </div>
+  );
 }
